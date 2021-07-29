49aec2179aa27b37821673d4878814966e7bbc40
bayzid23564 <83860351+bayzid23564@users.noreply.github.com>
2021-07-29 12:48:14 +0600

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,7 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-curl -O https://raw.githubusercontent.com/bayzid23564/RDPByBayzidGaming/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/bayzid23564/RDPByBayzidGaming/main/configfornewfreevps.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://github.com/bayzid23564/RDPByBayzidGaming/raw/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://github.com/bayzid23564/RDPByBayzidGaming/raw/main/EVKey64.exe > out.txt 2>&1
 net user administrator fmcpe@123 /add >nul
